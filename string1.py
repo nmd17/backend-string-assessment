cf644153db75546851241f4103dbe21d2d72ae71
Nick D <nmdanner123@gmail.com>
2019-04-22 15:33:13 -0400

    initial

--- string1.py
+++ string1.py
@@ -24,8 +24,12 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-    """Your code goes here.  Edit this docstring."""
-    return
+    if(count >= 10):
+        return "Number of donuts: many"
+    else:
+        return "Number of donuts: " + str(count)
+
+
 
 
 # B. both_ends
@@ -34,8 +38,12 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    """Your code goes here.  Edit this docstring."""
-    return
+    if(len(s) == 1):
+        return ''
+    beg = s[:2]
+    end = s[-2:]
+    
+    return beg + end
 
 
 # C. fix_start
@@ -48,8 +56,9 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    """Your code goes here.  Edit this docstring."""
-    return
+    temp = s[1:]
+    new = temp.replace(s[0], "*")
+    return s[0] + new
 
 
 # D. MixUp
@@ -60,8 +69,9 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    """Your code goes here.  Edit this docstring."""
-    return
+    result = b[:2] + a[2:] + " " + a[:2] + b[2:]
+
+    return result
 
 
 # Provided simple test() function used in main() to print
